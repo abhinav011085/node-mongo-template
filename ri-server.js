689f7bfc9b6b8da97ef8bfeaeaca654854a85586
Abhinav <athawait5@gmail.com>
2023-02-03 16:02:05 +0530

    Created router folder to handle rutes separately

--- ri-server.js
+++ ri-server.js
@@ -2,33 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 require('dotenv').config()
 const logger = require('./logger/api.logger');
-
-const taskController = require('./controller/task.controller')
-
+const taskRouter = require('./router/task.routes');
 
 const app = express();
 
 app.use(bodyParser.json());
-
-app.get('/api/tasks', (req, res) => {
-    taskController.getTasks().then(data => res.json(data));
-});
-
-app.get('/api/tasks/:id', (req, res) => {
-    taskController.getTask(req.params.id).then(data => res.json(data));
-});
-
-app.post('/api/task', (req, res) => {
-    taskController.createTask(req.body.task).then(data => res.json(data));
-});
-
-app.put('/api/task/:id', (req, res) => {
-    taskController.updateTask(req.params.id, req.body.task).then(data => res.json(data));
-});
-
-app.delete('/api/task/:id', (req, res) => {
-    taskController.deleteTask(req.params.id).then(data => res.json(data));
-});
+app.use(taskRouter);
 
 app.get('/', (req, res) => {
     res.send(`<h1>Invalid Request</h1>`)
---
+++ router/task.routes.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+const taskController = require('../controller/task.controller')
+
+router.use(express.json());
+
+router.get('/api/tasks', (req, res) => {
+    taskController.getTasks().then(data => res.json(data));
+});
+
+router.get('/api/tasks/:id', (req, res) => {
+    taskController.getTask(req.params.id).then(data => res.json(data));
+});
+
+router.post('/api/task', (req, res) => {
+    taskController.createTask(req.body.task).then(data => res.json(data));
+});
+
+router.put('/api/task/:id', (req, res) => {
+    taskController.updateTask(req.params.id, req.body.task).then(data => res.json(data));
+});
+
+router.delete('/api/task/:id', (req, res) => {
+    taskController.deleteTask(req.params.id).then(data => res.json(data));
+});
+
+module.exports = router;
